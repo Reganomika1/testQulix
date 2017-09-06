df3e37ee1e61af78b9e9e226df0dcab0c50fcb22
Macbook <zakharsazanovets@outlook.com>
2017-09-06 23:57:47 +0300

    CoreData added

---
+++ TestProjectForQulix/CommitsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CommitsViewController.swift
+//  TestProjectForQulix
+//
+//  Created by Macbook on 06.09.17.
+//  Copyright © 2017 Macbook. All rights reserved.
+//
+
+import Foundation
---
+++ TestProjectForQulix/RepoTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  RepoTableViewCell.swift
+//  TestProjectForQulix
+//
+//  Created by Macbook on 06.09.17.
+//  Copyright © 2017 Macbook. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RepoTableViewCell: UITableViewCell {
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+    
+}
